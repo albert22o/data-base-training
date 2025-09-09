fb92366bfdcd21b3dca3803d8940e4239715a8ba
Albert <albertoganesan5@gmail.com>
2025-09-09 14:22:12 +0700

    LAB_3. Поправил основной запрос

--- lab3/Script.sql
+++ lab3/Script.sql
@@ -1,11 +1,11 @@
 --1. Вывести количество продавцов из городов, начинающихся на буквы I-N.
-select count(city) from sal where city~'^[I-N]'
+select count(city) from sal where city~'^[I-N]';
 --2. Вывести наименьший и наибольший вес из таблицы продуктов.
 select max(weight),min(weight) from prod;
 --3. Вывести для каждого продукта наименьший номер продавца, у которого этот продукт продавался.
-select prod.name,min(ord.snum) from ord inner join prod on ord.pnum = prod.pnum group by prod.name;
+select prod.name,min(ord.snum) from ord,prod where ord.pnum = prod.pnum group by prod.name;
 --4. Вывести число уникальных продавцов, которые продавали товары покупателям с номерами 2005 и 2004. 
-select distinct count(snum) from ord where cnum = 2005 or cnum = 2004;
+select count(DISTINCT snum) from ord where cnum = 2005 or cnum = 2004;
 --5. Вывести для каждого покупателя его номер и общее количество проданных продуктов, если это количество не меньше, чем 15.
 select cnum,count(pnum) from ord group by cnum having count(pnum) >= 15;
 --6. Вывести количество заказов для всех уникальных пар продавец-продукт, между которыми осуществлялись заказы.
